accac0ff406503ccb644fa307ad43f66b1294244
Kavindu Methmal <kavindumethmal1234@gmail.com>
2025-07-02 22:40:44 +0530

    User validation.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class UserController extends Controller
+{
+    public function userSignIn(Request $request): JsonResponse
+    {
+       $User = User::where('email',$request->email)->first();
+
+        if(! $User)
+        {
+            return response()->json([
+                'message' => 'unsuccess'
+            ]);
+        }
+
+        if ($this->isValidateCredential($request->all() , $User)){
+            return response()->json([
+                'message' => 'success']);
+        }
+        return response()->json(['message' => 'not found']);
+
+    }
+    public function isValidateCredential(array $request,User $User)
+    {
+       return $User->email===$request['email'] && Hash::check($request['password'],$User->password);
+    }
+
+
+}
