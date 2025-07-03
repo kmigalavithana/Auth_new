52f5adbd055efa51b288478b109c50997189c2d8
Kavindu Methmal <kavindumethmal1234@gmail.com>
2025-07-03 16:49:56 +0530

    Get the sign in from devide the code and define types for state.

--- resources/frontend/src/Componets/signIn/SignIn.tsx
+++ resources/frontend/src/Componets/signIn/SignIn.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
+import SigninForm from "./SigninForm.tsx";
+import type { ISignInState } from "../../Utilities/types/signIn/SignIn";
+
 
 const SignIn: React.FC = () => {
+
+    const [signDtails, setSignDtails] = useState<ISignInState>({
+        email: '',
+        password: ''
+    });
+
     return (
-        <div>
-            hlooooooo
-        </div>
+       <SigninForm />
     );
 };
 
---
+++ resources/frontend/src/Utilities/types/signIn/SignIn.d.ts
@@ -0,0 +1,4 @@
+export interface ISignInState{
+    email: string,
+    password: string,
+}
